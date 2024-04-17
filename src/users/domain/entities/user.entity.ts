3e3e2ea731818d3da90f6f60b7228c296e619656
Daniel Oliveira <daniel.oliveira@alymente.com.br>
2024-04-17 19:40:28 -0300

    Update  methods.

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -7,22 +7,48 @@ export type UserProps = {
   createdAt?: Date
 }
 
+export type UserPropsUpdate = {
+  name: string
+  email: string
+}
+
 export class UserEntity extends Entity<UserProps> {
   constructor(public readonly props: UserProps, id?: string) {
     super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
+  update(props: UserPropsUpdate): void {
+    this.name = props.name
+    this.email = props.email
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name(){
     return this.props.name
+  }
+
+  private set name(value: string) {
+    this.props.name = value
   }
 
   get email(){
     return this.props.email
   }
 
+  private set email(value: string) {
+    this.props.email = value
+  }
+
   get password(){
     return this.props.password
+  }
+
+  private set password(value: string) {
+    this.props.password = value
   }
 
   get createdAt(){
